ff9181e7d54ec6a935a92102b821f229b83adda0
ut <ut@Albizia>
2018-09-23 12:22:15 +0200

    url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-// export const BACKEND_SERVER_URL = 'https://recognition-of-the-face-api.herokuapp.com';
-export const BACKEND_SERVER_URL = 'http://localhost:3001';
+export const BACKEND_SERVER_URL = 'https://recognition-of-the-face-api.herokuapp.com';
+// export const BACKEND_SERVER_URL = 'http://localhost:3001';
